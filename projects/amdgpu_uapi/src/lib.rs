3068f1b852356a605279650a2eff37a267ac803f
Rosa, Ada <arosa@amd.com>
2026-04-17 21:38:17 +0000

    ioctl

--- projects/amdgpu_uapi/src/lib.rs
+++ projects/amdgpu_uapi/src/lib.rs
@@ -1,10 +1,8 @@
-use std::mem::transmute;
-
-trait DecodeIoctl {
+pub trait DecodeIoctl {
     // it's not safe to decode an ioctl becuse the data could point anywhere
     unsafe fn decode_ioctl(nr: u32, args: &[u8]) -> Self;   
 }
 
-trait EncodeIoctl {
+pub trait EncodeIoctl {
     fn encode_ioctl(&self) -> (u32, &[u8]);
 }
