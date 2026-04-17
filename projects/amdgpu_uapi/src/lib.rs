f9f8b2d858cf4aaf4eec99dcceb2ef2c7576e626
Rosa, Ada <arosa@amd.com>
2026-04-17 21:38:13 +0000

    refactor: improve trait structure for DecodeIoctl and EncodeIoctl

--- projects/amdgpu_uapi/src/lib.rs
+++ projects/amdgpu_uapi/src/lib.rs
@@ -4,3 +4,7 @@ trait DecodeIoctl {
     // it's not safe to decode an ioctl becuse the data could point anywhere
     unsafe fn decode_ioctl(nr: u32, args: &[u8]) -> Self;   
 }
+
+trait EncodeIoctl {
+    fn encode_ioctl(&self) -> (u32, &[u8]);
+}
