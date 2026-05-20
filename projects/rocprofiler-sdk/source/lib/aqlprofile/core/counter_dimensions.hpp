052bc3feb17735f278a34aa9c1b0da5471508c9e
EvanPhoukong <Evan.Phoukong@amd.com>
2026-05-20 15:16:07 +0000

    gfx11/gfx12: use WGP attr (not SE) to set workgroup_processor dimension

--- projects/rocprofiler-sdk/source/lib/aqlprofile/core/counter_dimensions.hpp
+++ projects/rocprofiler-sdk/source/lib/aqlprofile/core/counter_dimensions.hpp
@@ -116,7 +116,7 @@ public:
         if(bIsGFX9)
             compute_unit = HasAttr(CounterBlockTcAttr) && shader_engine;
         else if(bIsGFX11 || bIsGFX12)
-            workgroup_processor = HasAttr(CounterBlockSqAttr);
+            workgroup_processor = HasAttr(CounterBlockWgpAttr);
 
         se_num  = pm4_factory->GetShaderEnginesNumber();
         sarrays = pm4_factory->GetShaderArraysNumber() * se_num;
